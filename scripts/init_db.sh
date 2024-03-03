d0a851b5729d35d19a718690d6867c9071fc6daa
Joshua Wootonn <joshuawootonn@gmail.com>
2024-03-03 09:31:55 -0500

    chore: update init_db.sh to include running the migrations

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,16 +2,16 @@ set -x
 set -eo pipefail
 
 if ! [ -x "$(command -v psql)" ]; then
-  echo >&2 "Error: psql is not installed."
-  exit 1
+	echo >&2 "Error: psql is not installed."
+	exit 1
 fi
 if ! [ -x "$(command -v sqlx)" ]; then
-  echo >&2 "Error: sqlx is not installed."
-  echo >&2 "Use:"
-  echo >&2 " cargo install --version='~0.7' sqlx-cli \
+	echo >&2 "Error: sqlx is not installed."
+	echo >&2 "Use:"
+	echo >&2 " cargo install --version='~0.7' sqlx-cli \
   --no-default-features --features rustls,postgres"
-  echo >&2 "to install it."
-  exit 1
+	echo >&2 "to install it."
+	exit 1
 fi
 
 # Check if a custom user has been set, otherwise default to 'postgres'
@@ -25,29 +25,26 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 # Check if a custom host has been set, otherwise default to 'localhost'
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
-
-# raunch postgres using Docker
-if [[ -z "${SKIP_DOCKER}" ]]
-then
-  docker run \
-  -e POSTGRES_USER=${DB_USER} \
-  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-  -e POSTGRES_DB=${DB_NAME} \
-  -p "${DB_PORT}":5432 \
-  -d postgres \
-  postgres -N 1000
-  # ^ Increased maximum number of connections for testing purposes
+# launch postgres using Docker
+if [[ -z "${SKIP_DOCKER}" ]]; then
+	docker run \
+		-e POSTGRES_USER=${DB_USER} \
+		-e POSTGRES_PASSWORD=${DB_PASSWORD} \
+		-e POSTGRES_DB=${DB_NAME} \
+		-p "${DB_PORT}":5432 \
+		-d postgres \
+		postgres -N 1000
+	# ^ Increased maximum number of connections for testing purposes
 fi
-
-
 
 # Keep pinging Postgres until it's ready to accept commands
 export PGPASSWORD="${DB_PASSWORD}"
 until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
-  >&2 echo "Postgres is still unavailable - sleeping"
-  sleep 1
+	>&2 echo "Postgres is still unavailable - sleeping"
+	sleep 1
 done
 
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
 sqlx database create
+sqlx migrate run
