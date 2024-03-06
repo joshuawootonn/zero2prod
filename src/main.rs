65c5a879899458e12cb9bab575ca56f77eb5eb53
Joshua Wootonn <joshuawootonn@gmail.com>
2024-03-06 07:49:31 -0500

    feat: chapter 3 complete - test isolation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use sqlx::PgPool;
+
 use crate::configuration::get_configuration;
 use crate::startup::run;
 use std::net::TcpListener;
@@ -9,9 +11,12 @@ mod startup;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect ot Postgres");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
 
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
