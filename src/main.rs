1ceaa561d0a151271070d8554855deee5cc626ac
Joshua Wootonn <joshuawootonn@gmail.com>
2024-03-07 08:29:12 -0500

    feat: add logging

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use sqlx::PgPool;
-
 use crate::configuration::get_configuration;
 use crate::startup::run;
+use env_logger::Env;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 mod configuration;
@@ -17,6 +17,8 @@ async fn main() -> Result<(), std::io::Error> {
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
+
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     run(listener, connection_pool)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,6 @@
-use std::net::TcpListener;
-
-use actix_web::{dev::Server, web, App, HttpServer};
+use actix_web::{dev::Server, middleware::Logger, web, App, HttpServer};
 use sqlx::PgPool;
+use std::net::TcpListener;
 
 use crate::routes::{health_check, subscribe};
 
@@ -9,6 +8,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
